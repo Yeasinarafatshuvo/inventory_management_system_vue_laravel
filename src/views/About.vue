59073cf7e91d584f8256aa11cf5ccb2dadfd55a5
Yeasin Arafat <yeasinshuvo76@gmail.com>
2022-10-24 18:46:53 +0600

    added notification library notify

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="click_notification">click</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    click_notification() {
+      this.$notify({
+        title: "Important message",
+        text: "Hello user!",
+        type: "error",
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
